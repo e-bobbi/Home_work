afdf8bfab4369217c5dfdbc7904df4d335d52277
Евгений <enbob.tester@gmail.com>
2025-02-11 16:23:07 +0300

    Create 2_hw.py

---
+++ 2_hw.py
@@ -0,0 +1,17 @@
+def task_1() -> None:
+# Создаем пять переменных с разными типами данных
+    var_int = 42          # int
+    var_float = 3.14      # float
+    var_str = "Привет!"   # str
+    var_list = [1, 2, 3]  # list
+    var_bool = True       # bool
+
+# Выводим тип данных каждой переменной
+    print(type(var_int))
+    print(type(var_float))
+    print(type(var_str))
+    print(type(var_list))
+    print(type(var_bool))
+
+# Вызываем функцию
+task_1()
